496050030d527d78e4a338876b37b7cf9ebf33ac
BenkoBranislav111177 <xbenkob@stuba.sk>
2023-05-14 14:40:01 +0200

    Navbar and Example

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import React from "react";
 import Login from "./component/Login.jsx";
+import Example from "./component/example/example.jsx";
+import Navbar from './component/navbar/Navbar.jsx';
 
 export default function App() {
 
   return (
     <>
         <BrowserRouter>
+        <Navbar/>
+            
             <Routes>
-                <Route index element={<Login/>}/>
+                <Route index element={<Example/>}/>
+            </Routes>
+            <Routes>
+                <Route index element={<Example/>}/>
             </Routes>
         </BrowserRouter>
     </>
