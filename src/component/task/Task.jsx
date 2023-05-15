90f07d3a7f0e9ff647f5b69c470f2ac495e76cf7
krizek79 <krizan.matej79@gmail.com>
2023-05-16 01:37:52 +0200

    katex to show math in task descriptions

--- src/component/task/Task.jsx
+++ src/component/task/Task.jsx
@@ -1,10 +1,16 @@
 import React, {useEffect, useState} from 'react'
 import taskService from "../../service/TaskService.js"
+import katex from "react-dom/server";
+import TextWithLatex from "./TextWithLatex.jsx";
+import MathInput from "./MathInput.jsx";
 
 export default function Task(props) {
 
     const [task, setTask] = useState({
-        description: "",
+        description: "\n" +
+            "    Nájdite prenosovú funkciu $F(s)=\\dfrac{Y(s)}{W(s)}$ pre systém opísaný blokovou schémou: \n" +
+            "\n" +
+            "    \\includegraphics{zadanie99/images/blokovka01_00002.jpg} \n",
         imageUrl: "../../../public/blokovka01_00002.jpg"
     })
 
@@ -13,15 +19,15 @@ export default function Task(props) {
     })
 
     useEffect(() => {
-        taskService.getTaskById(props.id)
-            .then(response => {
-                if (response.status === 200) {
-                    console.log(response.data)
-                }
-            })
-            .catch(e => {
-                console.log(e.response.status + ": " + e.response.data.message)
-            })
+        // taskService.getTaskById(props.id)
+        //     .then(response => {
+        //         if (response.status === 200) {
+        //             console.log(response.data)
+        //         }
+        //     })
+        //     .catch(e => {
+        //         console.log(e.response.status + ": " + e.response.data.message)
+        //     })
     }, [])
 
     function handleChange(e) {
@@ -40,32 +46,29 @@ export default function Task(props) {
             })
     }
 
+    function removeIncludeGraphics(str) {
+        const regex = /\\includegraphics\{[^}]*}/g;
+        return str.replace(regex, '');
+    }
+
     return (
         <div className="px-2 md:px-6 my-5 w-full text-slate-700 dark:text-dark flex flex-col items-center">
             <div className="max-w-xl text-left flex flex-col w-full items-center justify-center md:p-4 ">
                 <div
                     className="w-full rounded-xl flex-col xl:flex-row bg-white dark:bg-light shadow-md"
                 >
-                    <div className="w-full p-3 flex flex-col justify-between h-auto overflow-auto lg:h-auto">
-                        <h1 className="text-left text-sm md:text-lg font-bold leading-normal">
-                            1. Example
-                        </h1>
-                        <p className="text-sm">
-                            Nájdite prenosovú funkciu pre systém opísaný blokovou schémou:
+                    <div className="w-full p-6 flex flex-col justify-between h-auto overflow-auto lg:h-auto">
+                        <p className="">
+                            <TextWithLatex text={removeIncludeGraphics(task.description)}/>
                         </p>
                         {task.imageUrl && (
-                            <div className="mt-5 rounded-t-xl w-full h-64 shadow-sm">
+                            <div className="py-6 rounded-t-xl w-full h-64 shadow-sm">
                                 <img src={task.imageUrl} alt="Block" className="w-full h-full"/>
                             </div>
                         )}
                         <form className="w-full p-3 flex flex-col justify-between h-auto overflow-auto lg:h-auto">
-                            <div className="mt-5 relative z-0 col-span-2">
-                                <textarea
-                                    name="message" rows="3"
-                                    className="peer block w-full appearance-none border-0 border-b border-dark
-                                    bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-gray-600
-                                    focus:outline-none focus:ring-0" placeholder="Your sollution"
-                                ></textarea>
+                            <div id="mathInput" className="">
+                                <MathInput/>
                             </div>
                             <button
                                 type="submit"
